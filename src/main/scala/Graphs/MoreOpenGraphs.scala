58275475158eb43a01b883fc1164b210d715af9c
bmonteiro00 <bmonteiro00@gmail.com>
2019-08-11 23:55:48 -0300

    FanOutShape2 FanIntShape samples

--- src/main/scala/Graphs/MoreOpenGraphs.scala
+++ src/main/scala/Graphs/MoreOpenGraphs.scala
@@ -1,14 +1,15 @@
 package Graphs
 
+import java.util.Date
+
 import akka.actor.ActorSystem
-import akka.stream.{ActorMaterializer, ClosedShape, UniformFanInShape, scaladsl}
-import akka.stream.scaladsl.{GraphDSL, RunnableGraph, Sink, Source, ZipWith}
+import akka.stream.{ActorMaterializer, ClosedShape, FanOutShape2, UniformFanInShape, scaladsl}
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, RunnableGraph, Sink, Source, ZipWith}
 
 object MoreOpenGraphs extends App{
 
   implicit val system = ActorSystem("MoreOpenGraphs")
   implicit val materializer = ActorMaterializer()
-
 
   val max3StaticGraph = GraphDSL.create()
   {
@@ -42,7 +43,45 @@ object MoreOpenGraphs extends App{
       ClosedShape
     }
   )
+  //max3RunnableGraph.run()
 
-  max3RunnableGraph.run()
+  case class Transaction(id: String, source: String, recipient: String, amount: Int, date:Date)
+
+  val transactionSource = Source(List(
+    Transaction("1234567890", "Bruno", "Jose", 100, new Date),
+    Transaction("1234445555", "Jose", "Saidera", 100000, new Date),
+    Transaction("1234444444", "Bruno", "Seisa", 1000, new Date)))
+
+  val bankProcessor = Sink.foreach[Transaction](println)
+  val suspeciousTransactionProcessor = Sink.foreach[Transaction](transaction => println(s"Suspecious transaction: $transaction.id"))
+
+  val suspeciousBankTransactionStaticGraph = GraphDSL.create(){
+    implicit builder =>
+      import GraphDSL.Implicits._
+
+      val broadcast = builder.add(Broadcast[Transaction](2))
+      val suspeciousTxnFilter = builder.add(Flow[Transaction].filter(transaction => transaction.amount > 1000))
+      val transactionExtractor = builder.add(Flow[Transaction].map[Transaction](trans => trans))
+
+      broadcast.out(0) ~> suspeciousTxnFilter ~> transactionExtractor
+
+      new FanOutShape2(broadcast.in, broadcast.out(1), transactionExtractor.out)
+  }
+
+  val suspiciousRunnableGraph = RunnableGraph.fromGraph(
+    GraphDSL.create(){
+      implicit builder =>
+        import GraphDSL.Implicits._
+
+        val suspiciusTnxShape = builder.add(suspeciousBankTransactionStaticGraph)
+
+        transactionSource ~> suspiciusTnxShape.in
+        suspiciusTnxShape.out0 ~> bankProcessor
+        suspiciusTnxShape.out1 ~> suspeciousTransactionProcessor
+
+        ClosedShape
+    }
+  )
+  suspiciousRunnableGraph.run()
 }
 
